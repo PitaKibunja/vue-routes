f48eeeb34621eec705d9da617c72577040c2086f
DapinTechnologies <70751446+DapinTechnologies@users.noreply.github.com>
2021-06-23 16:25:24 +0300

    more fun with vue-routes

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+    <h2>Page not Found! maybe view our homepage  
+        <router-link to="/teams">Teams</router-link> 
+    </h2>
+</template>
--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to Team 2</router-link>
   </section>
 </template>
 
@@ -16,7 +17,8 @@
 import UserItem from '../users/UserItem.vue';
 
 export default {
-  inject:['users','teams']
+  inject:['users','teams'],
+  props:['teamId']
   ,
   components: {
     UserItem
@@ -27,8 +29,8 @@ export default {
       members: []
     };
   },
-  created(){
-    const teamId=this.$route.params.teamId
+  methods:{
+    loadTeamMembers(teamId){
     const selectedTeam=this.teams.find(team=>team.id === teamId)
 
     const members=selectedTeam.members
@@ -39,8 +41,19 @@ export default {
     }
     this.members=selectedMembers
     this.teamName=selectedTeam.name
+    }
   }
-};
+  ,
+  created(){
+    this.loadTeamMembers(this.teamId)
+  },
+  watch:{
+    teamId(newId){
+     
+     this.loadTeamMembers(newId)}
+    
+  }
+}
 </script>
 
 <style scoped>
